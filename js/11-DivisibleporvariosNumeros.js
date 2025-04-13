d06943de00be9bc92e02789d50acc47cc8909ab7
Santiago <concursos.utn.frt@gmail.com>
2025-04-13 17:41:23 -0300

    corregi el ejercicio 11

--- js/11-DivisibleporvariosNumeros.js
+++ js/11-DivisibleporvariosNumeros.js
@@ -1,7 +1,7 @@
 const numero1 = parseInt(prompt("Ingrese un número"));
 if(numero1 % 2 === 0 || numero1 % 3 === 0 || numero1 % 5 === 0 || numero1 % 7 === 0){
     if(numero1 % 2 === 0){
-        document.writeln("El ",numero1," es divisible por 2");
+        document.writeln("es divisible por 2");
     }
     if(numero1 % 3 === 0){
         document.writeln("es divisible por 3");
@@ -14,5 +14,5 @@ if(numero1 % 2 === 0 || numero1 % 3 === 0 || numero1 % 5 === 0 || numero1 % 7 ==
     }
 }
 else{
-    document.writeln("El ",numero1," no es divisible ni por 2, ni por 3, ni por 5, ni por 7");
+    document.writeln("no es divisible ni por 2, ni por 3, ni por 5, ni por 7");
 }
