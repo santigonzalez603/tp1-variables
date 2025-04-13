de9171cde4071445503865c5cfd6cf9d17c5ef70
Santiago <concursos.utn.frt@gmail.com>
2025-04-13 10:25:31 -0300

    realize los ultimos 3 ejercicios

---
+++ js/5-Descuento.js
@@ -0,0 +1,3 @@
+let total_compra = parseFloat(prompt("Ingrese el total de la compra"));
+let descuento = 0.15 * total_compra;
+document.writeln("El total de la compra es: " + (total_compra - descuento));
