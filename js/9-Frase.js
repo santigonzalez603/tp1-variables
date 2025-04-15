95b21c861e40360efbcc539f290d27878f0b0669
Santiago <concursos.utn.frt@gmail.com>
2025-04-15 17:35:28 -0300

    corregi un error del ejercicio 9

--- js/9-Frase.js
+++ js/9-Frase.js
@@ -1,8 +1,5 @@
-let text = prompt("Escriba un frase");
-let ntext = text.length;
-let i;
-for (i = 0; i < ntext; i++){
-    if(text.substr(i,1) === "a" || text.substr(i,1) === "e" || text.substr(i,1) === "i" || text.substr(i,1) === "o" || text.substr(i,1) === "u" || text.substr(i,1) === "A" || text.substr(i,1) === "E" || text.substr(i,1) === "I" || text.substr(i,1) === "O" || text.substr(i,1) === "U"){
-        document.writeln(text.substr(i,1))
-    }
-}
+const frase = prompt('Ingrese una frase') //lisandro
+console.log(frase.length)
+document.writeln(frase.charAt(1))
+document.writeln(frase.charAt(3))
+document.writeln(frase.charAt(7))
